f795c887f8559361c6caf4d136fe734001206947
sonseokmin <son1245960@gmail.com>
2024-09-07 23:12:48 +0900

    Initial commit with project setup

---
+++ BE/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const route = require("./route/index.js");
+
+const app = express();
+app.use(express.json());
+app.use(cors("*"));
+app.use("/", route);
+
+app.listen(3000, () => {
+  console.log("Community app listening on port 3000");
+});
