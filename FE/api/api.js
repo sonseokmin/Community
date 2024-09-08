9b9b2f236328f8777b4f5d151ee3abb223c774b6
sonseokmin <son1245960@gmail.com>
2024-09-09 00:44:15 +0900

    "FE 게시판 게시글 전체 조회 요청 API 수정"

--- FE/api/api.js
+++ FE/api/api.js
@@ -1,9 +1,15 @@
+// API 요청 파일
+
+// 기본 URL
 const API_BASE_URL = "http://127.0.0.1:3000";
 
+// 모든 게시글 조회 API
 export async function getPosts(path) {
+  // path === board명
   try {
-    const response = await fetch(`${API_BASE_URL}/${path}`);
-    return response;
+    const response = await fetch(`${API_BASE_URL}/${path}/list`);
+    const data = response.json();
+    return data;
   } catch (err) {
     console.log(`Fail to fetch ${path} board posts`);
   }
--- FE/scripts/main.js
+++ FE/scripts/main.js
@@ -1,28 +1,56 @@
+/* 게시판 공통 기능 정의 파일 */
+
+// API 요청 기능 로드
 import { getPosts } from "../api/api.js";
 
 // 현재 URL을 '/'로 분할하여 배열로 변환합니다.
 const urlArray = document.URL.split("/");
-// paths 객체 정의
-const paths = {
+
+// board 객체 정의
+const board = {
   "introduction.html": "introduction",
   "free.html": "free",
 };
 
+//board 저장 변수
 let matchedValue = null;
 
-// 배열에서 paths의 키가 포함된 요소를 찾습니다.
+// 배열에서 board의 키가 포함된 요소를 찾습니다.
 urlArray.forEach((part) => {
-  if (paths[part]) {
-    matchedValue = paths[part];
+  // board 객체가 존재하거나 board 값에 해당할 경우
+  if (board[part] || part === board[part]) {
+    matchedValue = board[part] || part; // board 값 또는 요소 할당
   }
 });
 
+// html 파일이 로드됐을 때
 document.addEventListener("DOMContentLoaded", async function () {
+  // board가 존재할 경우
   if (matchedValue) {
     try {
-      // getPosts 함수 호출
-      const data = await getPosts(matchedValue);
-      console.log(data);
+      // 전체 게시글 요청
+      const response = await getPosts(matchedValue);
+
+      // 게시글 리스트 태그 (로드된 게시글을 담을 태그)
+      const postList = document.getElementById("posts_list");
+
+      // board에 있는 게시글만큼 데이터 로드 (제목, 내용)
+      response.data.forEach((data) => {
+        const postTitle = document.createElement("a");
+        postTitle.innerText = data.title;
+        postTitle.className = "post";
+        postTitle.href = `../read/read.html?id=${data.id}`;
+
+        const postContent = document.createElement("span");
+        postContent.innerText = data.content;
+
+        const li = document.createElement("li");
+
+        li.appendChild(postTitle);
+        li.appendChild(postContent);
+
+        postList.appendChild(li);
+      });
     } catch (error) {
       console.error("Error fetching posts:", error);
     }
