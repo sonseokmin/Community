ba6706c707ff2bd2da5b43bad66a3d7b01025810
sonseokmin <son1245960@gmail.com>
2024-09-19 14:48:42 +0900

    BE 로그인&회원가입 기능 추가를 위한 파일 추가

---
+++ BE/route/userRoute.js
@@ -0,0 +1,8 @@
+/* 유저 관련 API 경로 정의 파일 */
+
+const express = require("express");
+const userController = require("../controller/userController.js");
+
+const router = express.Router();
+
+module.exports = router;
