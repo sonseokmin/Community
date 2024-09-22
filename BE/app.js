41af5de67ccb379082a3d0425e36bb9ea9db744c
sonseokmin <son1245960@gmail.com>
2024-09-23 02:28:04 +0900

    BE 로그인 기능&로그인 확인 기능 추가 및 쿠키 생성 기능 추가

--- BE/app.js
+++ BE/app.js
@@ -3,12 +3,19 @@
 require("dotenv").config({ path: "./.env.dev" });
 
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const route = require("./route/index.js");
 
 const app = express();
 app.use(express.json());
-app.use(cors("*"));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500", // 클라이언트 도메인
+    credentials: true, // 쿠키를 포함한 요청 허용
+  })
+);
 app.use("/", route);
 
 app.listen(3000, () => {
